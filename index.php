5007439a1dfb8cb46f3ffe8f4eb2ce7f805945d5
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 18:21:21 +0700

    sql injection

---
+++ index.php
@@ -0,0 +1,15 @@
+<!-- PHP SECURITY -->
+
+# SQL injection
+- write ( 1' OR '1'='1' ) in form input for get table row
+- avoid sql injection with (real_escape_string) in input text
+- more code for sql injection can check on w3shools
+
+2. htaccess
+3. user access permission
+4. password user
+5. CSRF
+6. XSS
+7. Error Reporting
+
+<!-- END PHP SECURITY -->
