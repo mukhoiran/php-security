0a0612187a3177cd33ea1b65f9123e72d773f1dd
mukhoiran <mkhoir.anam@gmail.com>
2019-04-13 18:33:33 +0700

    password encryption

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <!-- PHP SECURITY -->
 
-# SQL injection
+# SQL Injection
 - write ( 1' OR '1'='1' ) in form input for get table row
 - avoid sql injection with (real_escape_string) in input text
 - more code for sql injection can check on w3shools
 
+# Password User
+- encrypt password with md5 -> md5($password)
+- encrypt password with md5 -> hash('md5',$password)
+- encrypt password with sha256 -> hash('sha256',$password)
+- decrypt password in crackstation.net
+- best enscrypt is password_hash -> password_hash($password) -> php 5.5 or higer
+- decrypt password_hash -> password_verify($password, $hash) -> return true/false
+
 2. htaccess
 3. user access permission
-4. password user
 5. CSRF
 6. XSS
 7. Error Reporting
